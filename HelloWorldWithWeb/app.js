9d04bb87f57e649f18cd1dae535d01284560eddc
Khandakar Rashed Hassan <kh.rashed900@gmail.com>
2023-08-09 14:05:17 +0600

    Added Hello World code for web

---
+++ HelloWorldWithWeb/app.js
@@ -0,0 +1,4 @@
+var message = 'Hello World from TypeScript!';
+var heading = document.createElement("h1");
+heading.textContent = message;
+document.body.appendChild(heading);
---
+++ HelloWorldWithWeb/app.ts
@@ -0,0 +1,4 @@
+let message: string = 'Hello World from TypeScript!';
+let heading = document.createElement("h1");
+heading.textContent = message;
+document.body.appendChild(heading);
