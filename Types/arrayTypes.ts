8e813609c6ff6b4577ca25029f0d801d5ed0a435
Khandakar Rashed Hassan <kh.rashed900@gmail.com>
2023-08-17 17:08:52 +0600

    Added array types examples

---
+++ Types/arrayTypes.ts
@@ -0,0 +1,13 @@
+let names: string[] = [];
+names[0] = 'Muhsina';
+names.push('Protity');
+
+console.log(names);
+
+let namesLength = names.length;
+
+names.forEach(n => console.log(n));
+
+let scores: (string | number)[];
+scores = ['Two', 2, 'Plus', -4];
+console.log(scores);
