8872bcc8b611f3d8e4a581294cfcc3f9bb9144ca
Rashed Hassan <rhassan@wcgclinical.com>
2023-08-25 20:09:59 +0600

    added tuple types

---
+++ Types/tupleTypes.ts
@@ -0,0 +1,8 @@
+// the number and types needs to be defined first. 
+let skills: [string, number];
+skills = ["communication", 0];
+
+// optional syntax 
+let rgbColor: [number, number, number, string?];
+rgbColor = [20,128,0,"show"];
+rgbColor = [20,128,34];
